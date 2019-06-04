3a82da4af66b2449e6381763929fc1b0d1d63d9e
Lucas Mateus <contaprivy@gmail.com>
2019-06-04 11:15:24 -0300

    testando

--- teste.php
+++ teste.php
@@ -1,18 +1,15 @@
 <?php
 
 //Arrumado!
-<<<<<<< HEAD
+ 
 echo "Hello World";
-No dia de hoje
-=======
+
 echo "Hello World"
 echo "Mis um cmoentario, TRABALHA GALERA"
 
-<<<<<<< HEAD
->>>>>>> f93778be094a23524e6610c0a1f306f56f1198d2
-=======
+
 //Lucas comentário
 echo "Teste de Lucas Mateus";
 
->>>>>>> a4e1391f21e2185655c160bb3fa857605454b2f9
+
 ?>
